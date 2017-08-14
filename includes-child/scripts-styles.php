8e36901a214b82a83732ff5e9b66d117dd3a03a1
Neil Gee <neil@wpbeaches.com>
2017-08-14 10:00:09 +1000

    remove later jquery, clash with themer, more mobile CSS

--- includes-child/scripts-styles.php
+++ includes-child/scripts-styles.php
@@ -5,17 +5,8 @@
  * @package beavertron
  */
 
-add_action('wp_enqueue_scripts', 'bt_jquery_enqueue');
-// Add later jQuery
- function bt_jquery_enqueue() {
-	wp_deregister_script( 'jquery' );
-	wp_register_script( 'jquery', ( 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js' ),  array(), '2.2.4', true );
-	wp_enqueue_script( 'jquery' );
-}
 
-
-
-// Load BB Child theme style sheet later so we beat out all the other guys.
+ // Load BB Child theme style sheet later so we beat out all the other guys.
 //add_action( 'wp_enqueue_scripts', 'FLChildTheme::enqueue_scripts', 1000 );
 add_action( 'wp_enqueue_scripts', 'bt_scripts_styles', 1000 ); // All the rest load before.
 /**
@@ -23,15 +14,24 @@ add_action( 'wp_enqueue_scripts', 'bt_scripts_styles', 1000 ); // All the rest l
  */
 function bt_scripts_styles() {
 	// wp_enqueue_style( 'googlefonts' , '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,600,700,300,800', array(), '2', 'all' );
-		wp_enqueue_style( 'fl-beavertron-theme', get_stylesheet_directory_uri() . '/style.css' );
-		wp_enqueue_script( 'remove-mobile', FL_CHILD_THEME_URL . '/js/remove-mobile.js', array(), '1.6', true );
-		wp_enqueue_style( 'remove-mobile-inbuilt' , FL_CHILD_THEME_URL . '/css/remove-mobile.css', array() , '1.6.1', 'all' );
-		
-        // wp_dequeue_style( 'foundation-icons' );
-        // wp_dequeue_style( 'font-awesome' );
-        // wp_dequeue_style( 'dashicons' );
+	wp_enqueue_style( 'fl-beavertron-theme', get_stylesheet_directory_uri() . '/style.css' );
+	wp_enqueue_script( 'remove-mobile', FL_CHILD_THEME_URL . '/js/remove-mobile.js', array(), '1.6', true );
+	wp_enqueue_style( 'remove-mobile-inbuilt' , FL_CHILD_THEME_URL . '/css/remove-mobile.css', array() , '1.6.1', 'all' );
+	
+	// wp_dequeue_style( 'foundation-icons' );
+	// wp_dequeue_style( 'font-awesome' );
+	// wp_dequeue_style( 'dashicons' );
 
 }
+
+// add_action( 'wp_enqueue_scripts', 'bt_jquery_enqueue' );
+// Disabled as causes issue with Beaver Themer // Add later jQuery
+ function bt_jquery_enqueue() {
+	wp_deregister_script( 'jquery' );
+	wp_register_script( 'jquery', ( 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js' ),  array(), '2.2.4', true );
+	wp_enqueue_script( 'jquery' );
+}
+
 
 //add_action( 'wp_enqueue_scripts', 'bt_ie_styles', 999 );	//IE conditional styles load last
 /**
