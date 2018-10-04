cd5947d7b53643f3a123fb0a349a2697ddd10218
Neil Gee <neil@wpbeaches.com>
2018-10-05 06:07:51 +1000

    break point tweak, add custom font filter added, CSS tweaks

--- includes-child/beaverbuilder.php
+++ includes-child/beaverbuilder.php
@@ -31,10 +31,10 @@
 }
 
 
-add_filter( 'fl_builder_font_families_system', 'bt_added_fonts_plugin' );
+//add_filter( 'fl_builder_font_families_system', 'bt_added_fonts_plugin' );
 /**
  * Add fonts to BB Module font dropdown selection
- * Adding  System Font UI Stack
+ * Example below
  * @since 1.0.0
  */
 function bt_added_fonts_plugin( $system ) {
@@ -47,7 +47,23 @@ function bt_added_fonts_plugin( $system ) {
 				"900"
             )
     );
-    
+    return $system;
+}
+//add_filter( 'fl_theme_system_fonts', 'bt_added_fonts' );
+/**
+ * Add fonts to BB Theme
+ * Adding Fonts to Customizer - example below
+ * @since 1.0.0
+ */
+function bt_added_fonts( $system ) {
+    $system[ '-apple-system' ] = array(
+			"fallback" => "BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif",			
+            "weights"  => array(
+                "300",
+                "400",
+                "700",
+            )
+    );
     return $system;
 }
 
@@ -64,7 +80,7 @@ function wb_builder_register_settings_form_short( $form, $id ) {
     $form['title'] = 'Beavertron Global Settings';
     $form['tabs']['general']['sections']['rows']['fields']['row_width']['default'] = '1200';    
 	$form['tabs']['general']['sections']['responsive']['fields']['responsive_breakpoint']['default'] = '767';
-	$form['tabs']['general']['sections']['responsive']['fields']['medium_breakpoint']['default'] = '1024';
+	$form['tabs']['general']['sections']['responsive']['fields']['medium_breakpoint']['default'] = '1023';
 	   
    } 
    
