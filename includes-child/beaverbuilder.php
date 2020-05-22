50ae14cdf56ddd8d28c0e423b0c04b6e6d49af9c
Neil Gee <neil@wpbeaches.com>
2020-05-22 11:47:16 +1000

    set rows to full by default

--- includes-child/beaverbuilder.php
+++ includes-child/beaverbuilder.php
@@ -101,7 +101,7 @@ function wb_builder_register_settings_form_short( $form, $id ) {
     $form['tabs']['general']['sections']['rows']['fields']['row_width']['default'] = '1200';    
 	$form['tabs']['general']['sections']['responsive']['fields']['responsive_breakpoint']['default'] = '767';
 	$form['tabs']['general']['sections']['responsive']['fields']['medium_breakpoint']['default'] = '1023';
-	   
+    $form['tabs']['general']['sections']['rows']['fields']['row_width_default']['default'] = 'full';
    } 
    
    return $form;
