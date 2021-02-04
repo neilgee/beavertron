2d60db23a125d56e36f1c5df79a89908080ada40
Neil Gee <neil@wpbeaches.com>
2021-02-04 16:00:16 +1100

    disable jetpack messages and xmlrpc

--- includes-child/dashboard.php
+++ includes-child/dashboard.php
@@ -82,6 +82,12 @@ add_filter( 'auto_theme_update_send_email', '__return_false' );
 // Disable application passwords introduced in WP 5.6.
 add_filter( 'wp_is_application_passwords_available', '__return_false' );
 
+// Disable xmlrpc
+add_filter( 'xmlrpc_enabled', '__return_false' );
+
+// Disable JetPack Messages
+add_filter( 'jetpack_just_in_time_msgs', '__return_false', 99 );
+
  
 add_action( 'admin_head', 'bt_hide_tabs' );
 /**
