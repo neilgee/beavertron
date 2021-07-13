9dbcc1adc10783c4670397d7cee519c8c0f3464f
Neil Gee <neil@wpbeaches.com>
2021-07-13 17:42:39 +1000

    add GF filter

--- includes-child/gravity.php
+++ includes-child/gravity.php
@@ -4,3 +4,16 @@
  * @since 1.0.0
  */
 add_filter( 'gform_enable_field_label_visibility_settings', '__return_true' );
+
+/**
+ * Filters for returning user to same place on page to see form submission notification
+ * Either for all or for specific form
+ * @since 1.0.0
+ */
+
+/*The following example enables the confirmation anchor on all forms. */
+//add_filter( 'gform_confirmation_anchor', '__return_true' );
+
+
+/* The following example enables the confirmation anchor for form with ID 5. */
+//add_filter( 'gform_confirmation_anchor_5', '__return_true' );
